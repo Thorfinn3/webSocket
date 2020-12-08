a8d5a92c33b6e2be145f6d971b80b6f8e06bf9f1
thorfinn <charles.picard3@gmail.com>
2020-12-08 22:47:23 +0100

    add messages

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
