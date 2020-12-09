5611f03451aba893bffd6eba5c88f04234ad9657
thorfinn <charles.picard3@gmail.com>
2020-12-09 14:35:40 +0100

    remove jquery

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//Initialisation et import des variables
 let Santa = require('./santa_class.js');
 
 let sweden = {	
@@ -20,37 +21,47 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+//Creation du socket 
 const io = socketIO(server);
 
+//Initialisation du bot
 var bot = new rivescript();
 
-bot.loadDirectory("rive").then(loading_done).catch(loading_error);
- 
+//Chargement du fichier de bot
+bot.loadDirectory("rive").then(loading_done).catch(loading_error); 
 bot.loadFile("rive/client.rive").then(loading_done).catch(loading_error);
  
+//Fonction de verification que le chargement du bot est bon
 function loading_done() {
   console.log("Bot has finished loading!");
- 
   bot.sortReplies();
 }
  
+//Fonction d'affichage d'un message d'erreur lorsque le bot est mal chargé
 function loading_error(error, filename, lineno) {
   console.log("Error when loading files: " + error);
 }
 
+//Ecoute des connections des clients sur le socket
 io.on('connection', (socket) => {
   console.log('Client connected');
 
+  //Ecoute des messages reçus par le client
   socket.on('msg', (msg) => {
     console.log('msg: ' + msg);
+
+    //Reccuperation de la reponse du bot rivescript
     bot.reply("client", msg).then(function(reply) {
       console.log("The bot says: " + reply);
+
+      //Envoi de la reponse du bot au client
       io.emit('river', "The bot says: " + reply);
     });
   });
 
+  //Gestion de la deconnection du client
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+//Envoi regulie de la position du Santa
 setInterval( () => io.emit('santa', JSON.stringify(mySanta.getPosition())), 5000);
